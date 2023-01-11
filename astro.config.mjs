e39ef0e0012f3afc3447667ad0379f221a68d658
Daniel Toint <Gedztodu@gmail.com>
2023-01-11 15:06:15 +0200

    Add digest styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,22 +8,18 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
 import addClasses from 'rehype-add-classes';
 
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), image(), tailwind()],
+  integrations: [react(), image(), tailwind(), mdx()],
   markdown: {
-      extendDefaultPlugins: true,
-      rehypePlugins: [
-          [
-              addClasses,
-              {
-                  h1: 'text-4xl text-red-600 font-rowdies'
-              }
-          ]
-      ]
+    extendDefaultPlugins: true,
+    rehypePlugins: [[addClasses, {
+      h1: 'text-4xl text-red-600 font-rowdies'
+    }]]
   }
 });
