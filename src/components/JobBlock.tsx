a7cbb002116d9a000bbd77c6240f67f463eeee4a
Daniel Toint <Gedztodu@gmail.com>
2023-01-12 09:49:14 +0200

    Add second them styling

--- src/components/JobBlock.tsx
+++ src/components/JobBlock.tsx
@@ -10,11 +10,11 @@ interface Props {
 
 const JobBlock: React.FC<Props> = ({title, position, picture, dates, duration, link, description}) => {
     return (
-    <section className="bg-zinc-700 p-8 flex flex-col mb-16 w-10/12 mx-auto">
+    <section className="bg-orange-100 dark:bg-zinc-600 rounded-lg text-zinc-900 dark:text-slate-50 p-8 flex flex-col mb-16 w-10/12 mx-auto transition-colors duration-500">
         <h2 className="text-2xl text-center mb-4">{title}</h2>
 
-        <img src={picture} alt=""  className="w-64 h-64 block mx-auto mb-8"/>
-        <span className="text-center text-gray-300">{position}</span>
+        <img src={picture} alt=""  className="w-64 h-64 block mx-auto mb-8 border-orange-500 border-2"/>
+        <span className="text-center text-gray-500 dark:text-gray-300 text-transition duration-500">{position}</span>
         <span className="text-center">{dates}</span>
         <span className="text-center mb-4">{duration}</span>
         <p className="w-10/12 mx-auto">{description}</p>
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -10,13 +10,13 @@ interface Props {
 
 const ProjectCard: React.FC<Props> = ({title, description, link, image, rmm, status}) => {
     return (
-    <div className="p-6 bg-zinc-700 flex flex-row-reverse mx-auto w-1/2 rounded-md justify-between h-48">
+    <div className="p-6 bg-orange-100 dark:bg-zinc-600 text-zinc-900 transition-colors duration-500     dark:text-slate-50 flex flex-row-reverse mx-auto w-1/2 rounded-md justify-between h-48">
     <div className="flex flex-row-reverse justify-end">
         <div className="pl-8 w-8/12">
             <h2 className="mb-4 text-2xl">{title}</h2>
             <p>{description}</p>
         </div>
-        <a href={link} ><img src={image} alt=""  className="w-32 h-32"/></a>
+        <a href={link} ><img src={image} alt=""  className="w-32 h-32 border-orange-500 border-2"/></a>
     </div>
   
     <div className="order-first text-center ml-8 text-sm">
