ec3393018909a9da651fecc8932699baeef7d398
Daniel Toint <Gedztodu@gmail.com>
2023-01-14 11:37:36 +0200

    Add responsiveness

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -10,20 +10,20 @@ interface Props {
 
 const ProjectCard: React.FC<Props> = ({title, description, link, image, rmm, status}) => {
     return (
-    <div className="p-6 bg-orange-100 dark:bg-zinc-600 text-zinc-900 transition-colors duration-500     dark:text-slate-50 flex flex-row-reverse mx-auto w-1/2 rounded-md justify-between h-48">
-    <div className="flex flex-row-reverse justify-end">
-        <div className="pl-8 w-8/12">
-            <h2 className="mb-4 text-2xl">{title}</h2>
-            <p>{description}</p>
+    <div className="p-6 bg-orange-100 dark:bg-zinc-600 text-zinc-900 transition-colors duration-500 dark:text-slate-50 flex flex-col-reverse sm:flex-row-reverse mx-auto rounded-md justify-between sm:w-1/2">
+    <div className="flex flex-col-reverse sm:flex-row-reverse sm:justify-end items-center">
+        <div className="mt-4 sm:mt-0 sm:pl-8 sm:w-8/12">
+            <h2 className="mb-4 text-2xl text-center sm:text-left">{title}</h2>
+            <p className="text-center sm:text-left font-rubik">{description}</p>
         </div>
         <a href={link} ><img src={image} alt=""  className="w-32 h-32 border-orange-500 border-2"/></a>
     </div>
   
-    <div className="order-first text-center ml-8 text-sm">
+    <div className="order-first text-center sm:ml-8 text-sm mt-2 sm:mt-0">
         <span>Status <br /><span className={status === "Active" ? "text-green-600" : "text-red-600"}>{status}</span> </span>
     </div>
 
-    <div className="order-first text-center text-sm">
+    <div className="order-first text-center text-sm mt-2 sm:mt-0">
         {rmm > 0 && <span>RMM <br />$ {rmm}</span>}
     </div>
 </div>)
