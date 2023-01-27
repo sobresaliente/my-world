19603f2ae9be7ea7bf6a5bc97cf6f0f8fdd4a469
Daniel Toint <Gedztodu@gmail.com>
2023-01-27 18:59:31 +0200

    Implement convertkit

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -1,12 +1,28 @@
 import { useState } from "react";
-
+import { convertKitFormId, convertKitKey } from '../../env.js'
 const Newsletrer = () => {
     const [email, setEmail] = useState('');
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
-        
+        const body = JSON.stringify({ email, api_key: convertKitKey});
+        const headers = new Headers({
+            'Content-Type': 'application/json; charset=utf-8',
+          });
+
+        try {
+        await fetch(`https://api.convertkit.com/v3/forms/${convertKitFormId}/subscribe`, {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            headers,
+            body,
+        });
+        setEmail('');
+        } catch(e) {
+            console.log(e.message);
+        }
     }
 
     return  <>
@@ -16,7 +32,7 @@ const Newsletrer = () => {
              <input type="email" id="email" className="block text-black dark:bg-dark-bg-secondary bg-bg-secondary rounded-xl mt-4 w-10/12 sm:w-4/12 mx-auto h-12 sm:mx-0" value={email} onChange={(event) => setEmail(event.target.value)} />
 
 
-            <button type="submit" className="mt-4 dark:bg-dark-bg-accent bg-bg-accent rounded-lg p-2" >Submit</button>
+            <button type="submit" className="mt-4 dark:bg-dark-bg-accent bg-bg-accent rounded-lg p-2">Submit</button>
         </form>
         </>
 };
