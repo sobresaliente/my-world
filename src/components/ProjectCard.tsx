75ca20ccf834e0ddda4628eff4868b91581a9cea
Daniel Toint <Gedztodu@gmail.com>
2023-01-27 20:16:37 +0200

    Add color fonts

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -12,7 +12,7 @@ const ProjectCard: React.FC<Props> = ({title, description, link, image, rmm, sta
     return (
     <div className="p-6 bg-bg-secondary dark:bg-dark-bg-secondary text-zinc-900 transition-colors duration-500 dark:text-slate-50 flex flex-col-reverse sm:flex-row-reverse mx-auto rounded-md justify-between sm:w-1/2 mt-8">
     <div className="flex flex-col-reverse sm:flex-row-reverse sm:justify-end items-center">
-        <div className="mt-4 sm:mt-0 sm:pl-8 sm:w-8/12">
+        <div className="mt-4 sm:mt-0 sm:pl-8 sm:w-8/12 text-dark-text-primary">
             <h2 className="mb-4 text-2xl text-center sm:text-left mt-2">{title}</h2>
             <p className="text-center sm:text-left font-rubik h-24">{description}</p>
         </div>
@@ -20,7 +20,7 @@ const ProjectCard: React.FC<Props> = ({title, description, link, image, rmm, sta
     </div>
   
     <div className="order-first text-center sm:ml-8 text-sm mt-4 sm:mt-2">
-        <span>Status <br /><span className={status === "Active" ? "text-green-600" : "text-red-600"}>{status}</span> </span>
+        <span className="text-dark-text-primary">Status <br /><span className={status === "Active" ? "dark:text-green-600 text-green-400" : "text-red-600"}>{status}</span> </span>
     </div>
 
     <div className="order-first text-center text-sm mt-2 sm:mt-0">
