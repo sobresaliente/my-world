86acb9b474a58faeea5240a0445588c64199d8ee
Daniel Toint <Gedztodu@gmail.com>
2023-01-29 14:39:37 +0200

    Add hard coded currently working

---
+++ src/components/ActivityBadge.tsx
@@ -0,0 +1,12 @@
+const ActivityBadge = () => {
+  return (
+    <section className='w-full'>
+      <h3 className='text-left text-xl mt-8'>Current activity:</h3>
+
+      <div className='flex relative dark:bg-dark-bg-accent bg-bg-accent w-full rounded-xl mt-4 text-left text-white h-24 justify-center sm:w-10/12'>
+        <div className='text-2xl h-1/3 my-auto '>Coding that blog</div>
+      </div>
+    </section>
+  );
+};
+export default ActivityBadge;
